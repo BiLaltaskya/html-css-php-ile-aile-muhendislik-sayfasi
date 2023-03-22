d802d8075e1d2ff0eb48dc60b3c9ee1d3ecafe0b
Bilal <110530920+BiLaltaskya@users.noreply.github.com>
2023-03-22 17:52:41 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taşkaya bilgisayar ve yazılım</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Andada+Pro:wght@600&family=Cormorant+Garamond:wght@500&family=Roboto:wght@900&display=swap" rel="stylesheet">
+</head>
+<script src="https://kit.fontawesome.com/2b643263b5.js" crossorigin="anonymous"></script>
+<body>
+   <section id ="menu">
+       <div id ="logo">Taşkaya Bilgisayar & Yazılım</div>
+<nav>
+    <a href=""><i class="fa-solid fa-house-user ikon"></i>Anasayfa</a>
+    <a href=""><i class="fa-solid fa-circle-info ikon"></i>Hakkımızda</a>
+    <a href=""><i class="fa-sharp fa-solid fa-people-group ikon"></i>Ekip</a>
+    <a href=""><i class="fa-solid fa-tty ikon"></i>İletişim</a>
+</nav>
+   </section>
+<section id="anasayfa">
+<div id="black">
+</div>
+<div id="içerik">
+<h2>Taşkaya Bilgisayar & Yazılım</h2>
+<hr width="300 "align="left">
+<p>
+    Web Tasarımı, Web Site Tasarımı, Hastane Web Sistesi, Okul Web Sitesi tasarımı, Doktor web site tasarımı,
+     Web Site Tasarım Hizmetleri, Web Site Güncelleme, Web Tasarımcısı, Kurumsal Web Tasarımı, 
+    Kurumsal Web Site Tasarımcısı, Dinamik Web Site Tasarımı, 
+    Web, Grafik, Seo, Seo Reklamları,
+     Seo Reklam Çalışmaları, Dijital Pazar, Web Site tasarım hizmetleri, web çalışmaları, google bölgesel seo, seo ustası, seo hocası, web hizmetleri, web reklam hizmetleri, site tasarımı,
+      en yakın web site tasarımcısı, kurumsal web sitesi, web site tasarımı, kurumsal web sitesi tasarımı,
+     hazır web sitesi satın al, web sitesi yapan firmalar, profesyonel web sitesi, web site tasarım firması.
+</p>
+</div>
+</section>
+
+<section id="hakkimizda">
+<h3>Hakkımızda</h3>
+<div id="container">
+<div id="sol">
+<h5 id="h5sol">
+    Teknoloji dünyasının parlayan yıldızı olmak, farklı
+     sektörlerde hizmet veren müşterilerimizle geleceği tasarlamak, 
+     ülkemizde ve yurtdışında liderliğimizi daha ileriye taşımak.
+</h5>
+<div id="sag">
+<span>H</span>
+<p id="psag">
+    edeflerimiz doğrultusunda tutku ve özenle çalışıyoruz. İş ortaklarımız ve 
+    çözüm ortaklarımızla birlikte olan bağımızı daima canlı tutarak işlerin çeviklik ilkesi ile ilerlemesini sağlıyoruz.
+     Geleceği tasarlamak adına kendimizi sürekli geliştiriyor ve bilgi birikimlerimizi sürekli artıyoruz.
+</p>
+</div>
+<img src="resimler/hakkımızda.jpg" alt="" class="img-fluid mt100">
+<p id="pson">
+    Taşkaya Bilgisayar & Yazılım Yazılım Hizmetleri olarak 2011 yılında teknolojik dönüşüm ve yazılım alanında hizmet vermek amacıyla kurulduk.
+    Profesyonel iş anlayışımızla, insana ve teknolojiye verdiğimiz değer doğrultusunda uzman kadromuzla dijital 
+    dönüşüm ve yazılım alanında işletmelere yönelik çözümler ve yenilikler sunmaya devam etmekteyiz. 
+    İşletmelere yönelik; ERP entegrasyonları, Web yazılımları, hızlı satış yazılımları, mobil yazılımlar, el terminali yazılımları, e-ticaret entegrasyonları,
+     pazaryeri entegrasyonları, pos entegrasyonları,
+    banka entegrasyonları ile hizmet ve servis yelpazemizi genişlettik.
+    Taşkaya Bilgisayar & Yazılım Hizmetleri olarak alanında lider firmalarla çalışmakta, teknoloji sektöründe güçlü yerimizi korumaktayız.
+</p>
+</div>
+</div>
+</section>
+
+<section id="ekip">
+<div class="container">
+<h3 id="ekiph3">Ekip</h3>
+
+<div class="sutun-sol-sag">
+<img src="resimler/ekip1.jpeg" alt="" class="img-fluid oval">
+<h4 class="ekipisim"> Bilal Taşkaya
+</h4>
+<p class="ekipp"> Back end  </p>
+<div class="ekip-ikon">
+<a href="#"><i class="fa-brands fa-facebook social" ></i></a>
+<a href="#"><i class="fa-brands fa-instagram  social"></i></a>
+<a href="#"><i class="fa-brands fa-whatsapp social"></i></a>
+</div>
+</div>
+<div class="sutun">
+    <img src="resimler/ekip3.jpeg" alt="" class="img-fluid oval">
+    <h4 class="ekipisim"> Göksel Taşkaya
+    </h4>
+    <p class="ekipp">  Front End</p>
+    <div class="ekip-ikon">
+    <a href="#"><i class="fa-brands fa-facebook social" ></i></a>
+    <a href="#"><i class="fa-brands fa-instagram  social"></i></a>
+    <a href="#"><i class="fa-brands fa-whatsapp social"></i></a>
+    </div>
+    </div>
+    <div class="sutun-sol-sag">
+        <img src="resimler/ekip2.jpeg" alt="" class="img-fluid oval">
+        <h4 class="ekipisim"> Mazlum Taşkaya
+        </h4>
+        <p class="ekipp">  Back End</p>
+        <div class="ekip-ikon">
+        <a href="#"><i class="fa-brands fa-facebook social" ></i></a>
+        <a href="#"><i class="fa-brands fa-instagram  social"></i></a>
+        <a href="#"><i class="fa-brands fa-whatsapp social"></i></a>
+        </div>
+        
+        </div>
+</div>
+</section>
+<section id="iletişim">
+<div class="container">
+<h3 id="h3iletisim">İletişim</h3>
+<form action="index.php" method="post">
+<div id="iletisimopak">
+<div id="formgroup">
+    <div id="solform">
+        <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+        <input type="text" name="tel" placeholder="Telefon Numarası" required class="form-control">
+    </div>
+    <div id="sagform">
+        <input type="email" name="mail" placeholder="Email Adresiniz" required class="form-control">
+        <input type="konu" name="tel" placeholder="Konu Başlığı Giriniz" required class="form-control">
+    </div>
+
+    <textarea name="mesaj" id="" cols="30" placeholder="Mesajınız" rows="10" required class="form-control"></textarea>
+    <input type="submit" value="Gönder" name="buttonGonder">
+</div>
+
+<div id="adres">
+<h4 id="adresbaslik">ADRES
+</h4>
+<p class="adresp"> Velibaba mahallesi</p>
+<p class="adresp">Digor caddesi</p>
+<p class="adresp">Mahirbey sokak No:225</p>
+<p class="adresp">Tel No :05318305790</p>
+<p class="adresp"> Email:Taşkaya Bilgisayar & Yazılım@gmail.com</p>
+
+
+</div>
+</div>
+</form>
+<?php
+ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['buttonGonder'])){
+
+include("baglanti.php");    
+$adsoyad=$_POST['isim'];
+$telefon=$_POST['tel'];
+$email=$_POST['mail'];
+$konu=$_POST['konu'];
+$mesaj=$_POST['mesaj'];
+
+$ekle="INSERT INTO iletisim (adsoyad, telefon, email, konu, mesaj) VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+if($baglan->query($ekle)===TRUE)
+{
+
+  echo "<script> alert('mesajınız başarılı ile gönderilmiştir') </script>";  
+}
+}
+
+?>
+<footer>
+   <div id="copyright">
+    2023 Tüm Hakları Saklıdır
+    <div id="socialfooter">
+        <a href="#"><i class="fa-brands fa-facebook social" ></i></a>
+        <a href="#"><i class="fa-brands fa-instagram  social"></i></a>
+        <a href="#"><i class="fa-brands fa-whatsapp social"></i></a>
+    </div>
+    <a href="#menu"><i class="fa-sharp fa-solid fa-chevron-up" id="up"></i></a>
+   </div> 
+</footer>
+
+
+
+
+</div>
+</section>
+
+</body>
+</html>
+
+
